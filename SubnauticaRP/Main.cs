d46bb5f5c6d64963ef35bf23e838ae5afe68e30d
ahk1221 <ahmed16.ahk@gmail.com>
2018-05-23 13:43:15 +0500

    Add project files.

---
+++ SubnauticaRP/Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SubnauticaRP
+{
+    public class Main
+    {
+        private static DiscordRPC.RichPresence Presence;
+
+        public static void Patch()
+        {
+            // Initialize the client
+            InitDiscord();
+
+            // Set the presence
+            DiscordRPC.UpdatePresence(ref Presence);
+        }
+
+        public static void InitDiscord()
+        {
+            // Handlers
+            var eventHandlers = default(DiscordRPC.EventHandlers);
+            eventHandlers.readyCallback = ReadyCallback;
+            eventHandlers.disconnectedCallback = DisconnectedCallback;
+            eventHandlers.errorCallback = ErrorCallback;
+
+            // Initialize
+            DiscordRPC.Initialize("448509316450877458", ref eventHandlers, true, "264710");
+
+            // Presence
+            Presence = new DiscordRPC.RichPresence
+            {
+                largeImageKey = "subnautica_main",
+                state = "Test state",
+                details = "Test details",
+                largeImageText = "Test largeImageText"
+            };
+        }
+
+        private static void DisconnectedCallback(int errorCode, string message)
+        {
+            // Log errors
+            Console.WriteLine("Discord ERR: ERROR CODE " + errorCode);
+            Console.WriteLine(message);
+        }
+
+        private static void ErrorCallback(int errorCode, string message)
+        {
+            // Log errors
+            Console.WriteLine("Discord ERR: ERROR CODE " + errorCode);
+            Console.WriteLine(message);
+        }
+
+        private static void ReadyCallback()
+        {
+            // Log
+            Console.WriteLine("Discord is READY!");
+        }
+    }
+}
